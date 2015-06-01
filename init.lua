d1d06fff0bff3cec2c3d9f0d5baf6be759bbd6ba
user <nobody>
2015-06-01 11:11:53 -0700

    auto (/extra/user/packages/git/minetest/mods/item_drop/init.lua) 231 673 7532

--- init.lua
+++ init.lua
@@ -1,8 +1,4 @@
-if drops == nil then
-    drops = {}
-end
-
-local function iprint(...)
+function iprint(...)
     -- wtf
     local thingies = {...}
 
@@ -11,11 +7,6 @@ local function iprint(...)
     end
     print('\27[1m[item_drop] '..table.concat(thingies, ' '), '\27[m')
 end
-
-local movers = {}
-local immune = {}
-
-local removedAlreadyDammit = {}
 
 vector.fixedNormalize = function(v)
     assert(v)
@@ -34,6 +25,14 @@ if not vector.dot then
     end
 end
 
+local movers = {}
+local immune = {}
+
+local removedAlreadyDammit = {}
+
+if drops == nil then
+    drops = {}
+end
 
 local function removeObject(object)
     movers[object] = nil
@@ -57,7 +56,7 @@ local function pickup(player, inv, object, pickupRadius)
     if inv and inv:room_for_item("main", item) then
         inv:add_item("main", item)
         if object:get_luaentity().itemstring ~= "" then
-            minetest.sound_play("item_drop_pickup", {
+            core.sound_play("item_drop_pickup", {
                 to_player = player:get_player_name(),
                 gain = 0.4,
             })
@@ -159,80 +158,97 @@ local function moveTowards(object, player, pickupRadius, attractRadius)
     object:setacceleration(vector.multiply(direct,A))
 end
 
+local function toggler(t,name,new)
+   local old = t[name]
+   return function(enable)
+      if enable then
+         t[name] = new
+      else
+         t[name] = old
+      end
+   end
+end
 
-if minetest.setting_get("enable_item_pickup") == "true" then
-    local tickets = 0 -- XXX: oy vey
-    moveDelay = 0
-    minetest.register_globalstep(function(dtime)
-        -- it's much more efficient to just restart... no way to unregister_globalstep right?
-        if not minetest.setting_get("enable_item_pickup") then return end
-        moveDelay = moveDelay + dtime
-        local pickupRadius = tonumber(minetest.setting_get("pickup_radius"))
-        local attractRadius = tonumber(minetest.setting_get("attract_radius"))
-        if not pickupRadius then pickupRadius = 0.5 end
-        if not attractRadius then attractRadius = 3 end
+function setupItemPickup()
+   local tickets = 0 -- XXX: oy vey
+   moveDelay = 0
+   function pickupItem(dtime)
+         moveDelay = moveDelay + dtime
+         local pickupRadius = tonumber(core.setting_get("pickup_radius"))
+         local attractRadius = tonumber(core.setting_get("attract_radius"))
+         if not pickupRadius then pickupRadius = 0.5 end
+         if not attractRadius then attractRadius = 3 end
 
-        if moveDelay > 0.1 then
+         if moveDelay > 0.1 then
             moveDelay = 0
             -- correct your trajectory while moving
             for object,pair in pairs(movers) do
-                local player = pair[1]
-                moveTowards(object,player,pickupRadius,attractRadius)
+               local player = pair[1]
+               moveTowards(object,player,pickupRadius,attractRadius)
             end
-        end
-        for _, player in ipairs(minetest.get_connected_players()) do
-            if player:get_hp() > 0 or not minetest.setting_getbool("enable_damage") then
-                local playerPosition = player:getpos()
-                if playerPosition ~= nil then
-                    playerPosition.y = playerPosition.y+0.5
-                    local inv = player:get_inventory()
+         end
+         for _, player in ipairs(core.get_connected_players()) do
+            if player:get_hp() > 0 or not core.setting_getbool("enable_damage") then
+               local playerPosition = player:getpos()
+               if playerPosition ~= nil then
+                  playerPosition.y = playerPosition.y+0.5
+                  local inv = player:get_inventory()
 
-                    for _, object in ipairs(minetest.env:get_objects_inside_radius(playerPosition, 3)) do
-                        if (immune[object] == nil or immune[object] ~= player:get_player_name()) and 
-                            isGood(object) and
-                            inv and
-                            inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring))
-                            then
-                                local ticket = tickets
-                                movers[object] = {player,ticket}
-                                tickets = tickets + 1
-                                moveTowards(object, player, pickupRadius, attractRadius)
-                                -- make sure object doesn't push the player around!
-                                object:get_luaentity().physical_state = true
-                                object:get_luaentity().object:set_properties({
-                                    physical = false,
-                                    collide_with_objects = false,
-                                    weight = 0
-                                })
-                                -- pleeease no immortal orbiting entities
-                                -- unless you want them to be >:)
-                                minetest.after(30, function(object)
-                                    -- only if it's still moving
-                                    -- but what if it started moving a second time?
-                                    pair = movers[object]
-                                    if pair and pair[2] == ticket then
-                                        stop(object)
-                                    end
-                                end, object)
-                        end
-                    end
-                end
+                  for _, object in ipairs(core.env:get_objects_inside_radius(playerPosition, 3)) do
+                     if (immune[object] == nil or immune[object] ~= player:get_player_name()) and 
+                        isGood(object) and
+                        inv and
+                        inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring))
+                     then
+                        local ticket = tickets
+                        movers[object] = {player,ticket}
+                        tickets = tickets + 1
+                        moveTowards(object, player, pickupRadius, attractRadius)
+                        -- make sure object doesn't push the player around!
+                        object:get_luaentity().physical_state = true
+                        object:get_luaentity().object:set_properties({
+                              physical = false,
+                              collide_with_objects = false,
+                              weight = 0
+                                                                    })
+                        -- pleeease no immortal orbiting entities
+                        -- unless you want them to be >:)
+                        core.after(
+                           30,
+                           function(object)
+                              -- only if it's still moving
+                              -- but what if it started moving a second time?
+                              pair = movers[object]
+                              if pair and pair[2] == ticket then
+                                 stop(object)
+                              end
+                           end, object)
+                     end
+                  end
+               end
             end
-        end
-    end)
+         end
+   end
+   return function(enable)
+      if enable then
+         core.register_globalstep(pickupItem)
+      else
+         core.registered_globalsteps[pickupItem] = nil
+      end
+   end
+end
+drops.enableItemPickup = setupItemPickup()
+
+local function expireLater(expiration, obj)
+    core.after(expiration, removeObject, obj)
 end
 
-function expireLater(expiration, obj)
-    minetest.after(expiration, removeObject, obj)
-end
-
-if minetest.setting_get("enable_item_drops") == "true" then
-    local old_handle_node_drops = minetest.handle_node_drops
-
-    function new_handle_node_drops(pos, drops, digger)
+drops.enableItemDrop = toggler(
+   core,'handle_node_drops',
+   function new_handle_node_drops(pos, drops, digger)
         local inv
         -- the digger might be a node, like a constructor
-        if minetest.setting_getbool("creative_mode") and digger and digger:is_player() then
+        if core.setting_getbool("creative_mode") and digger and digger:is_player() then
             inv = digger:get_inventory()
         end
         for _, item in ipairs(drops) do
@@ -247,7 +263,7 @@ if minetest.setting_get("enable_item_drops") == "true" then
             -- Only drop the item if not in creative, or if the item is not in creative inventory
             if not inv or not inv:contains_item("main", ItemStack(name)) then
                 for i=1, count do
-                    local obj = minetest.env:add_item(pos, item)
+                    local obj = core.env:add_item(pos, item)
                     if obj ~= nil then
                         local x = math.random(1, 5)
                         if math.random(1, 2) == 1 then
@@ -261,12 +277,12 @@ if minetest.setting_get("enable_item_drops") == "true" then
                         -- (still falling though)
                         obj:setvelocity({x=1/x, y=obj:getvelocity().y, z=1/z})
 
-                        if minetest.setting_get("remove_items") and tonumber(minetest.setting_get("remove_items")) then
+                        if core.setting_get("remove_items") and tonumber(core.setting_get("remove_items")) then
 
                             local lua = obj:get_luaentity()
-                            lua.age = minetest.get_gametime()
+                            lua.age = core.get_gametime()
                             lua.alreadyActivated = true
-                            local expiration = tonumber(minetest.setting_get("remove_items"))
+                            local expiration = tonumber(core.setting_get("remove_items"))
                             expireLater(expiration, obj)
                         end
                     end
@@ -275,18 +291,8 @@ if minetest.setting_get("enable_item_drops") == "true" then
         end
         -- the items have been dropped. Don't use builtin/item.lua or it could put the items
         -- into an inventory! (see quarry)
-        -- return old_handle_node_drops(pos, drops, digger)
+        -- return handle_node_drops.old(pos, drops, digger)
     end
-
-    function checkSetting(pos, drops, digger)
-        if minetest.setting_get("enable_item_drops") == "true" then
-            return new_handle_node_drops(pos, drops, digger)
-        else
-            return old_handle_node_drops(pos, drops, digger)
-        end
-    end
-    minetest.handle_node_drops = checkSetting
-
 end
 
 -- we will hold the age as a property of the lua object
@@ -305,100 +311,161 @@ end
 -- spawned, then expireLater would get called twice, if
 -- we didn't set an alreadyActivated flag.
 
+local itemType = core.registered_entities['__builtin:item']
 
+drops.enableItemDecay =
+   (function()
+         local activate = toggler(
+            itemType,'on_activate',
+            function new_on_activate(lua, staticdata, dtime_s)
+               local info = {}
+               if string.len(staticdata)>0 then
+                  info = core.deserialize(staticdata)
+               end
+               if info == nil then
+                  error('another module is using staticdata in a way we cannot use! we found '..staticdata)
+                  info = {}
+               end
+               if old_on_activate then
+                  if info._hack_other_info then
+                     staticdata = info._hack_other_info
+                  end
+               end
+               on_activate.old(lua,staticdata, dtime_s)
+               local obj = lua.object
 
-local itemType = minetest.registered_entities['__builtin:item']
+               local expiration = tonumber(core.setting_get("remove_items"))
+               if expiration == nil then
+                  return
+               end
+               
+               if lua.alreadyActivated then return end
+               lua.alreadyActivated = true
+               local now = core.get_gametime()
+               
+               -- all items decay. The ones without an age are assigned the current
+               -- time when discovered.
 
-local old_on_activate = itemType.on_activate
-itemType.on_activate = function(lua, staticdata)
-    local info = {}
-    if string.len(staticdata)>0 then
-        info = minetest.deserialize(staticdata)
-    end
-    if info == nil then
-        error('another module is using staticdata in a way we cannot use! we found '..staticdata)
-        info = {}
-    end
-    if old_on_activate then
-        if info._hack_other_info then
-            staticdata = info._hack_other_info
-        end
-    end
-    old_on_activate(lua,staticdata)
-    local obj = lua.object
+               if info.age == nil then
+                  lua.age = core.get_gametime()
+                  expireLater(expiration, obj);
+                  return
+               end
+               
+               lua.age = info.age
+               
+               local timeLeft = expiration - (now - info.age)
+               
+               if timeLeft <= 0 then
+                  removeObject(obj)
+               else
+                  -- wait the rest of the time left, then expire
+                  expireLater(timeLeft, obj)
+               end
+         end)
+         local getstatic = toggler(
+            itemType,'get_staticdata',
+            function(lua)                            
+               local info = {}
+               if getstatic.old then
+                  info = getstatic.old(lua)
+               end
+               if type(info)~='table' then
+                  info = {_hack_other_info=info}
+               end
+               info.age = lua.age
+               return core.serialize(info)
+         end)
+         -- anyone who has ideas how not to completely replace core.item_drop let me know
+         -- the existing one doesn't have the hooks!
+         -- the 'immune' is needed so you don't pickup items you drop before they even appear
+         
+         local itemdrop = toggler(
+            core,'item_drop',
+            function new_item_drop(itemstack, dropper, pos)
+               if dropper.get_player_name then
+                  local v = dropper:get_look_dir()
+                  local p = {x=pos.x+v.x, y=pos.y+1.5+v.y, z=pos.z+v.z}
+                  local obj = core.add_item(p, itemstack)
+                  if obj then
+                     immune[obj] = dropper:get_player_name()
+                     core.after(math.random(3,5), function(obj)
+                                   immune[obj] = nil
+                                                  end, obj)
+                     v.x = v.x*2
+                     v.y = v.y*2 + 1
+                     v.z = v.z*2
+                     obj:setvelocity(v)
+                  end
+               else
+                  core.add_item(pos, itemstack)
+               end
+               return ItemStack("")
+         end)
+         return function(enable)
+            activate(enable)
+            getstatic(enable)
+            itemdrop(enable)
+         end
+   end)();
 
-    local expiration = tonumber(minetest.setting_get("remove_items"))
-    if expiration == nil then
-        return
-    end
+local modes = {}
 
-    if lua.alreadyActivated then return end
-    lua.alreadyActivated = true
-    local now = minetest.get_gametime()
-
-    -- all items decay. The ones without an age are assigned the current
-    -- time when discovered.
-
-    if info.age == nil then
-        lua.age = minetest.get_gametime()
-        expireLater(expiration, obj);
-        return
-    end
-
-    lua.age = info.age
-
-    local timeLeft = expiration - (now - info.age)
-
-    if timeLeft <= 0 then
-        removeObject(obj)
-    else
-        -- wait the rest of the time left, then expire
-        expireLater(timeLeft, obj)
-    end
+function setupSetting(name,default)
+   local setting = 'enable_item_'..name
+   local toggler = drops['enableItem'..name:title()]
+   if nil == core.setting_get(setting) then
+      core.setting_setbool(setting,default)
+      core.setting_save()
+   end
+   local enabled = default
+   modes[name] = function(enable)
+      if enable and enabled then return end
+      if (not enable) and (not enabled) then return end
+      enabled = enable
+      toggler(enable)
+      core.setting_setbool(setting,enable)
+      core.setting_save()
+      return true
+   end
 end
-local old_get_staticdata = itemType.get_staticdata
-itemType.get_staticdata = function(lua)
-    local info = {}
-    if old_get_staticdata then
-        info = old_get_staticdata(lua)
-    end
-    if type(info)~='table' then
-        info = {_hack_other_info=info}
-    end
-    info.age = lua.age
-    return minetest.serialize(info)
+
+setupSetting('drop',false)
+setupSetting('pickup',true)
+setupSetting('decay',true)
+
+local pattern = nil
+for n,_ in pairs(modes) do
+   if pattern == nil then
+      pattern = mode
+   else
+      pattern = pattern .. '|' .. mode
+   end
 end
+pattern = '(no)?('..pattern..')'
 
-if minetest.setting_get("enable_item_pickup") == "true" then
-    -- anyone who has ideas how not to completely replace minetest.item_drop let me know
-    -- the existing one doesn't have the hooks!
-    -- the 'immune' is needed so you don't pickup items you drop before they even appear
-    minetest.item_drop = function(itemstack, dropper, pos)
-        if dropper.get_player_name then
-                local v = dropper:get_look_dir()
-                local p = {x=pos.x+v.x, y=pos.y+1.5+v.y, z=pos.z+v.z}
-                local obj = minetest.add_item(p, itemstack)
-                if obj then
-                        immune[obj] = dropper:get_player_name()
-                        minetest.after(math.random(3,5), function(obj)
-                            immune[obj] = nil
-                        end, obj)
-                        v.x = v.x*2
-                        v.y = v.y*2 + 1
-                        v.z = v.z*2
-                        obj:setvelocity(v)
-                end
-        else
-                minetest.add_item(pos, itemstack)
-        end
-        return ItemStack("")
-    end
-end
+core.register_chatcommand(
+   "itemdrop",{
+      params = pattern,
+      description = "Adjust item drop settings",
+      func = function(player,param)
+         local enable,action = param:match(pattern)
+         if action == nil then
+            core.chat_send_player(player,"/itemdrop "..pattern)
+            return
+         end
+         enable = (enable ~= 'no')
+         if modes[action](enable) then
+            if enable then
+               enable = 'enabled'
+            else
+               enable = 'disabled'
+            end
+            core.chat_send_player(
+               player,
+               "Successfully "..enable.." "..action.." on items.")
+         end
+      end
+})
 
-
--- other mods ignore this, so why shouldn't we?
--- if minetest.setting_get("log_mods") then
---    minetest.log("action", "item_drop loaded")
-    iprint('loaded 0.1')
--- end
-
+iprint('loaded 0.1')
